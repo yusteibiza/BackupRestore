f98902768da6e0e6e68b551a9c66d77a8c77c14a
Antonio José Yuste López <yusteibiza@hotmail.com>
2019-11-22 11:59:14 +0100

    Proyecto inicial

---
+++ ServBackupRestore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ServBackupRestore
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ ServBackupRestore/Service1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ServBackupRestore
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+            this.ServiceName = "Backup&Restore";
+            
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+        protected override void OnPause()
+        {
+            
+        }
+    }
+}
